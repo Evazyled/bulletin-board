59518cfed503c7c8ece826d2224a199881174de7
Vladimir Danilov <evazyled@gmail.com>
2022-04-26 15:07:54 +0300

    auth form

--- src/components/common/RegForm/RegForm.tsx
+++ src/components/common/RegForm/RegForm.tsx
@@ -10,104 +10,33 @@ type RegFormPropsType = {
   subTitle: string;
 };
 
-const RegForm: React.FC<RegFormPropsType> = ({ title, subTitle }) => (
+const RegForm: React.FC<RegFormPropsType> = ({ title, subTitle, children }) => (
   <div className={style.regForm}>
     <div className={style.text}>
       <div className={style.regForm__title}>{title}</div>
       <div className={style.regForm__subTitle}>{subTitle}</div>
     </div>
-
-    <div className={style.regForm__body}>
-      <ul>
-        <div className={style.list__item}>
-          <li>
-            <NavLink
-              to="/registration"
-              className={({ isActive }) => (isActive ? style.active_link : style.not_active_link)}>
-              Регистрация
-            </NavLink>
-          </li>
-        </div>
-        <div className={style.list__item}>
-          <li>
-            <NavLink
-              to="/auth"
-              className={({ isActive }) => (isActive ? style.active_link : style.not_active_link)}>
-              Авторизация
-            </NavLink>
-          </li>
-        </div>
-      </ul>
-      <Form>
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Поле Имя обязательно',
-            },
-          ]}>
-          <Input placeholder="Имя" />
-        </Form.Item>
-        <Form.Item
-          name="usersurname"
-          rules={[
-            {
-              required: true,
-              message: 'Поле Фамилия не может быть пустым',
-            },
-          ]}>
-          <Input placeholder="Фамилия" />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: 'email',
-              message: 'Введите правильный Email',
-            },
-            {
-              required: true,
-            },
-          ]}>
-          <Input placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-          hasFeedback>
-          <Input.Password placeholder="Пароль" />
-        </Form.Item>
-        <Form.Item
-          name="confirm"
-          dependencies={['password']}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-
-                return Promise.reject(new Error('Введённые пароли должны совпадать'));
-              },
-            }),
-          ]}>
-          <Input.Password placeholder="Повторите пароль" />
-        </Form.Item>
-        <Checkbox>Checkbox</Checkbox>
-        <Button title="Создать аккаунт" />
-      </Form>
-    </div>
+    <ul>
+      <div className={style.list__item}>
+        <li>
+          <NavLink
+            to="/registration"
+            className={({ isActive }) => (isActive ? style.active_link : style.not_active_link)}>
+            Регистрация
+          </NavLink>
+        </li>
+      </div>
+      <div className={style.list__item}>
+        <li>
+          <NavLink
+            to="/auth"
+            className={({ isActive }) => (isActive ? style.active_link : style.not_active_link)}>
+            Авторизация
+          </NavLink>
+        </li>
+      </div>
+    </ul>
+    <div className={style.regForm__body}>{children}</div>
   </div>
 );
 
--- src/components/pages/RegistrationPage/RegistrationPage.tsx
+++ src/components/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,7 +1,81 @@
 import React from 'react';
-
+import { Form, Input, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
+import Button from '../../common/Button/Button';
 import RegForm from '../../common/RegForm/RegForm';
 
-const RegistrationPage = () => <RegForm title="Hello, World" subTitle="Создайте аккаунт" />;
+const RegistrationPage = () => (
+  <RegForm title="Hello, World" subTitle="Создайте аккаунт">
+    <Form>
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Поле Имя обязательно',
+          },
+        ]}>
+        <Input placeholder="Имя" />
+      </Form.Item>
+      <Form.Item
+        name="usersurname"
+        rules={[
+          {
+            required: true,
+            message: 'Поле Фамилия не может быть пустым',
+          },
+        ]}>
+        <Input placeholder="Фамилия" />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            type: 'email',
+            message: 'Введите правильный Email',
+          },
+          {
+            required: true,
+          },
+        ]}>
+        <Input placeholder="Email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+        hasFeedback>
+        <Input.Password placeholder="Пароль" />
+      </Form.Item>
+      <Form.Item
+        name="confirm"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject(new Error('Введённые пароли должны совпадать'));
+            },
+          }),
+        ]}>
+        <Input.Password placeholder="Повторите пароль" />
+      </Form.Item>
+      <Checkbox>Checkbox</Checkbox>
+      <Button title="Создать аккаунт" />
+    </Form>
+  </RegForm>
+);
 
 export default RegistrationPage;
