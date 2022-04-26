eabf031125cd4f7f6f398b243da60d5c8315a49a
Vladimir Danilov <evazyled@gmail.com>
2022-04-26 15:16:44 +0300

    change search

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -1,17 +1,19 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 import FoundButton from '../FoundButton/FoundButton';
-
-// const Search = () => <input type="search" className={style.content} />;
 import style from './Search.module.scss';
 import SearchIcon from './SearchIcon';
 
-const Search = () => (
-  <div className={style.content}>
-    <SearchIcon />
-    <input type="search" />
-    <FoundButton title="Искать" />
-  </div>
-);
+const Search = () => {
+  const [search, setSearch] = useState('');
+  console.log(search);
+
+  return (
+    <div className={style.content}>
+      <SearchIcon />
+      <input type="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <FoundButton title="Искать" />
+    </div>
+  );
+};
 
 export default Search;
