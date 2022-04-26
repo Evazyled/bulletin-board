d6af333ee2b332688d2c62e4025fbd14a22b7c28
Vladimir Danilov <evazyled@gmail.com>
2022-04-26 16:15:08 +0300

    to come in

--- src/components/common/UserStatus/UserStatus.tsx
+++ src/components/common/UserStatus/UserStatus.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import UserStatusIcon from './UserStatusIcon/UserStatus';
 import UserStatusTitle from './UserStatusTitle/UserStatusTitle';
 
@@ -7,7 +8,9 @@ import style from './User.status.module.scss';
 const UserStatus = () => (
   <div className={style.content}>
     <UserStatusIcon />
-    <UserStatusTitle title="Войти" />
+    <NavLink to="/auth">
+      <UserStatusTitle title="Войти" />
+    </NavLink>
   </div>
 );
 
