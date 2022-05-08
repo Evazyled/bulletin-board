d6a51801e26a99bcc203676088deb2ee1307d91c
Vladimir Danilov <evazyled@gmail.com>
2022-05-08 23:50:42 +0300

    layout tablet

--- src/components/common/Header/UserStatus/UserStatus.tsx
+++ src/components/common/Header/UserStatus/UserStatus.tsx
@@ -7,55 +7,44 @@ import UserStatusTitle from './UserStatusTitle/UserStatusTitle';
 
 import style from './UserStatus.module.scss';
 
-const UserStatus = ({ onMouseEnter, onClick }: any) => {
-  const width = document.documentElement.clientWidth;
-  if (width <= 768) {
-    return (
-      <div className={style.userStatus} onMouseEnter={onMouseEnter}>
-        <UserStatusIcon />
-        <NavLink to="/auth">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <g opacity="0.4">
-              <path
-                d="M3 12H21"
-                stroke="#2A2F37"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 6H21"
-                stroke="#2A2F37"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M3 18H21"
-                stroke="#2A2F37"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </g>
-          </svg>
-        </NavLink>
-      </div>
-    );
-  }
-  return (
-    <div className={style.userStatus} onMouseEnter={onMouseEnter}>
-      <UserStatusIcon />
-      <NavLink to="/auth">
-        <UserStatusTitle title="Войти" />
-      </NavLink>
-    </div>
-  );
-};
+const UserStatus = ({ onMouseEnter, onClick }: any) => (
+  <div className={style.userStatus} onMouseEnter={onMouseEnter}>
+    <UserStatusIcon />
+    <NavLink to="/auth">
+      <UserStatusTitle title="Войти" />
+      <svg
+        className={style.burger}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <g opacity="0.4">
+          <path
+            d="M3 12H21"
+            stroke="#2A2F37"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M3 6H21"
+            stroke="#2A2F37"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M3 18H21"
+            stroke="#2A2F37"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+      </svg>
+    </NavLink>
+  </div>
+);
 
 export default UserStatus;
--- src/components/common/Header/UserStatus/UserStatusTitle/UserStatusTitle.tsx
+++ src/components/common/Header/UserStatus/UserStatusTitle/UserStatusTitle.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import style from './UserStatusTitle.module.scss';
 
 type UserStatusTitleType = {
   title: string;
 };
 
-const width = window.innerWidth;
-
-const UserStatusTitle = ({ title }: UserStatusTitleType) => <span>{title}</span>;
+const UserStatusTitle = ({ title }: UserStatusTitleType) => (
+  <span className={style.userStatusTitle}>{title}</span>
+);
 
 export default UserStatusTitle;
