5b4b18cae4d8c945cafff2b4b47aff09bc69bed9
Vladimir Danilov <evazyled@gmail.com>
2022-05-14 20:00:12 +0300

    usermenu hover

--- src/components/common/Header/UserStatus/UserStatus.tsx
+++ src/components/common/Header/UserStatus/UserStatus.tsx
@@ -8,47 +8,49 @@ import UserStatusTitle from './UserStatusTitle/UserStatusTitle';
 import style from './UserStatus.module.scss';
 import UserMenu from '../UserMenu/UserMenu';
 
-const UserStatus = ({ onMouseEnter }: any) => {
+const UserStatus = () => {
   const isAuth = true;
   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className={style.userStatus}>
-      <UserStatusIcon />
-      <UserStatusTitle title={isAuth ? 'Профиль' : 'Войти'} />
-      <NavLink to="/auth">
-        <svg
-          className={style.burger}
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <g opacity="0.4">
-            <path
-              d="M3 12H21"
-              stroke="#2A2F37"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M3 6H21"
-              stroke="#2A2F37"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M3 18H21"
-              stroke="#2A2F37"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </g>
-        </svg>
-      </NavLink>
+      <div className={style.info}>
+        <UserStatusIcon />
+        <UserStatusTitle title={isAuth ? 'Профиль' : 'Войти'} />
+        <NavLink to="/auth">
+          <svg
+            className={style.burger}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <g opacity="0.4">
+              <path
+                d="M3 12H21"
+                stroke="#2A2F37"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M3 6H21"
+                stroke="#2A2F37"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M3 18H21"
+                stroke="#2A2F37"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </g>
+          </svg>
+        </NavLink>
+      </div>
       <UserMenu />
     </div>
   );
